a569d44e711a9f23122a7d1ce80e66d601c24e71
yugoo90 <uosuji54@gmail.com>
2022-01-22 16:34:10 -0700

    fixed infinite loop

--- src/route_planner.cpp
+++ src/route_planner.cpp
@@ -80,10 +80,10 @@ std::vector<RouteModel::Node> RoutePlanner::ConstructFinalPath(RouteModel::Node 
     // TODO: Implement your solution here.
     while(current_node->parent != nullptr){
         path_found.push_back(*current_node);
+        distance += current_node->distance(*current_node->parent);
+        current_node = current_node-> parent;
     }
-    for(auto node : path_found){
-        distance += node.distance(*node.parent);
-    }
+    
     distance *= m_Model.MetricScale(); // Multiply the distance by the scale of the map to get meters.
     std::reverse(path_found.begin(), path_found.end());
     return path_found;
